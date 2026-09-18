3951b8e85fed8c85e80ad8a5001c068ea4436792
Artem Kuzko <artem.kuzko@datarobot.com>
2026-09-18 16:00:20 +0200

    238 2nd option

--- python/FLG_ALGO_SD_BEGINNERS/week_1_BigO_arrays/238_product_of_the_array/solution.py
+++ python/FLG_ALGO_SD_BEGINNERS/week_1_BigO_arrays/238_product_of_the_array/solution.py
@@ -3,14 +3,32 @@ def product_of_the_array(nums):
 
     # left pass
     l_accum = r_accum = 1
+    n = len(nums)
 
-    for i in range(1, len(nums)):
-        l_accum = nums[i - 1] * res[i - 1]
+    for i in range(1, n):
+        l_accum *= nums[i]
         res[i] = l_accum
-    
-    for j in range(len(nums) - 1, - 1, -1):
+
+    for j in range(n - 1, -1, -1):
+        res[j] = r_accum
+        r_accum *= nums[j]
+
+    return res
+
+def product_of_the_array2(nums):
+    res = [1] * len(nums)
+
+    # left pass
+    l_accum = r_accum = 1
+    n = len(nums)
+
+    for i in range(n):
+        res[i] = l_accum
+        l_accum *= nums[i]
+
+    for j in reversed(range(n)):
         res[j] *= r_accum
-        r_accum = r_accum * nums[j]
+        r_accum *= nums[j]
 
     return res
 
